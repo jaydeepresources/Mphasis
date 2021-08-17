d88f852f2d43d971d78071a67450c8a9561b6f9b
jaydeep apte <jaydeepresources@gmail.com>
2021-08-17 10:07:46 +0530

    Add files via upload

---
+++ src/app/model/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  userId: number;
+  username: string;
+  password: string;
+  name: string;
+
+  constructor() {
+    this.userId = 0;
+    this.username = '';
+    this.name = '';
+    this.password = '';
+  }
+}
